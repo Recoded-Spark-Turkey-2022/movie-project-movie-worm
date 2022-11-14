f72d47602ee7ac5d57937682a418efe55cdc86ce
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-11-14 15:07:13 +0300

    HTML & JavaScript | filtering function create

--- script.js
+++ script.js
@@ -125,10 +125,81 @@ homeBtn.addEventListener('click', () => {
   window.location.reload()
 })
 
+// Filter > relaese year slider function
+let slider1 = document.getElementById('yearRange')
+let output1 = document.getElementById('demo1')
+  output1.innerHTML = slider1.value
+
+  slider1.oninput = function(){
+    output1.innerHTML = this.value
+  }
+
+// Filter > rating slider function
+let slider2 = document.getElementById('ratingRange')
+let output2 = document.getElementById('demo2')
+  output2.innerHTML = slider2.value
+
+  slider2.oninput = function(){
+    output2.innerHTML = this.value
+  }
+
+// Filter Button function
+const filterBtn = document.getElementById('filterBtn')
+
+let selectedGenreId = ''
+
+filterBtn.addEventListener('click', () => {
+  const filterDiv = document.getElementById('filterDiv')
+
+  if(filterDiv.style.display == 'block'){
+    filterDiv.style.display = 'none'
+  }else{
+    filterDiv.style.display = 'block'
+
+          //Also fetch genres to show in filter box
+    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`)
+    .then(resp => resp.json())
+    .then(json => {
+      const genreListFilter = document.getElementById('genre-filter')
+      const genresListJson = json.genres
+      
+    
+      for(let i = 0; i < genresListJson.length; i++){
+    
+        const option = document.createElement('option')
+        option.value = genresListJson[i].id
+        option.textContent = genresListJson[i].name
+    
+        genreListFilter.appendChild(option)
+      }
+    
+      genreListFilter.addEventListener('change', (e) => {
+        selectedGenreId = e.target.value
+      })
+    })
+  }
+})
+
+// Find movies button function
+const findMoviesBtn = document.getElementById('giveMeMoviesBtn')
+  findMoviesBtn.addEventListener('click', () => {
+    const selectedYear = slider1.value
+    const selectedRate = slider2.value
 
 
+// If we want to see the movies AFTER a certain relase year we can use this fetch
+//https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${selectedYear}&vote_average.gte=${selectedRate}&with_genres=${selectedGenreId}&with_watch_monetization_types=flatrate
 
 
+//this is filtering only the selecter year's movies
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=${selectedYear}&vote_average.gte=${selectedRate}&with_genres=${selectedGenreId}&with_watch_monetization_types=flatrate`)
+    .then(resp => resp.json())
+    .then(json => {
 
+      CONTAINER.innerHTML =''
+      renderMovies(json.results)
+    })
+
+  })
 
 document.addEventListener("DOMContentLoaded", autorun);
